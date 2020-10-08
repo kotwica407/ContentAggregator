ec1a12564f082af38dfd29ef54dea14ae1e66162
Mateusz Adamowicz <mateusz.adamowicz.1994@gmail.com>
2020-10-08 18:30:30 +0200

    StartupExtensions.cs

---
+++ ContentAggregator.Web/Extensions/StartupExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContentAggregator.Web.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+
+        }
+    }
+}
