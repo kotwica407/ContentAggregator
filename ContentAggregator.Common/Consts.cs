45d23e11a39b1f6905cea5f6e2c8e742c04758de
Mateusz Adamowicz <mateusz.adamowicz.1994@gmail.com>
2020-10-24 11:07:34 +0200

    PictureRepository

--- ContentAggregator.Common/Consts.cs
+++ ContentAggregator.Common/Consts.cs
@@ -1,4 +1,6 @@
-﻿namespace ContentAggregator.Common
+﻿using System.Collections.Generic;
+
+namespace ContentAggregator.Common
 {
     public static class Consts
     {
@@ -9,5 +11,23 @@
         public const int PostTitleLength = 50;
         public const int PostContentLength = 2000;
         public const int CommentContentLength = 1000;
+
+        public static class Picture
+        {
+            public static readonly List<string> AvailableMimeTypes = new List<string>
+            {
+                ContentTypes.ImageJpeg,
+                ContentTypes.ImagePng
+            };
+        }
+
+        public static class ContentTypes
+        {
+            public const string ImagePng = "image/png";
+            public const string ImageJpeg = "image/jpeg";
+            public const string Pdf = "application/pdf";
+            public const string Json = "application/json";
+        }
+
     }
 }
--- ContentAggregator.Repositories/Pictures/IPictureRepository.cs
+++ ContentAggregator.Repositories/Pictures/IPictureRepository.cs
@@ -1,8 +1,12 @@
-﻿using ContentAggregator.Models.Model;
+﻿using System.Threading.Tasks;
+using ContentAggregator.Models.Model;
 
 namespace ContentAggregator.Repositories.Pictures
 {
-    public interface IPictureRepository : ICrudRepository<Picture>
+    public interface IPictureRepository
     {
+        Task CreateOrUpdate(Picture picture);
+        Task<Picture> Get(string userId);
+        Task Delete(string userId);
     }
 }
